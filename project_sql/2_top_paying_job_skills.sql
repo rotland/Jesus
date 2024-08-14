83ee62e99c5e4d8c8299783e553f8f53920945cc
rotland <65259178+rotland@users.noreply.github.com>
2024-08-14 09:51:45 +0100

    update new query

---
+++ project_sql/2_top_paying_job_skills.sql
@@ -0,0 +1,22 @@
+-- What skills are required for the top-paying data analyst jobs?
+
+WITH top_paying_job as (
+SELECT
+    job_id,
+    job_title,
+    salary_year_avg,
+    name as company_name
+FROM job_postings_fact
+    LEFT JOIN company_dim on job_postings_fact.company_id = company_dim.company_id
+WHERE job_title_short= 'Data Analyst' AND
+    job_location = 'Anywhere' AND
+    salary_year_avg IS NOT NULL
+ORDER BY salary_year_avg DESC
+LIMIT 10
+)
+select top_paying_job.*,
+        skills
+from top_paying_job 
+INNER JOIN skills_job_dim on top_paying_job.job_id = skills_job_dim.job_id
+INNER JOIN skills_dim on skills_job_dim.skill_id = skills_dim.skill_id
+ORDER BY salary_year_avg DESC;
